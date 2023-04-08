086bf7dfd8dfd4b11ba96447cd14ebe88369789c
JustCris <cristian.scapin654@icloud.com>
2023-04-08 17:55:27 +0200

    refactor: [tw_to_scss] extracted functions

--- tw_to_scss.py
+++ tw_to_scss.py
@@ -4,6 +4,9 @@ import sys
 import io
 import json
 
+DEFAULT_DICT_FILE_NAME = "tw_to_scss.json"
+DEFAULT_OUT_FILT_NAME = "out.scss"
+
 media_queries = {
     "sm": "@media (min-width: 640px) {$}",
     "md": "@media (min-width: 768px) {$}",
@@ -16,6 +19,52 @@ states = {
     "hover": ":hover {$}",
     "focus": ":focus {$}",
 }
+
+
+def convert_media_query(
+    splitted_class: [str], twtoscss_dict: dict, media_queries: dict
+):
+    return (
+        media_queries[splitted_class[0]].replace(
+            "$", f"\n  {twtoscss_dict[splitted_class[1]]}\n"
+        )
+        + "\n"
+    )
+
+
+def convert_state(splitted_class: [str], twtoscss_dict: dict, states: dict):
+    return (
+        "&"
+        + states[splitted_class[0]].replace(
+            "$", f"\n  {twtoscss_dict[splitted_class[1]]}\n"
+        )
+        + "\n"
+    )
+
+
+# detect if class is using media queries or states and convert
+# accordingly
+def convert_derived_state(
+    tw_class: str,
+    twtoscss_dict: dict,
+    media_queries: dict,
+    states: dict,
+    out: io.StringIO,
+):
+    splitted = tw_class.split(":")
+    if splitted[0] in media_queries:
+        scss_to_write = convert_media_query(splitted, twtoscss_dict, media_queries)
+    elif splitted[0] in states:
+        scss_to_write = convert_state(splitted, twtoscss_dict, states)
+    print(scss_to_write)
+    out.write(scss_to_write)
+
+
+# convert simple tw class to scss
+def convert_tw_class(tw_class: str, twtoscss_dict: dict, out: io.StringIO):
+    scss_to_write = f"{twtoscss_dict[tw_class]}\n"
+    print(scss_to_write)
+    out.write(scss_to_write)
 
 
 def convert_line_and_write(line: str, twtoscss_dict: dict, out: io.StringIO):
@@ -31,29 +80,9 @@ def convert_line_and_write(line: str, twtoscss_dict: dict, out: io.StringIO):
         # i could've checked splitted lenght but this if
         # seems more readable to me
         if ":" in tw_class:
-            splitted = tw_class.split(":")
-            if splitted[0] in media_queries:
-                scss_to_write = (
-                    media_queries[splitted[0]].replace(
-                        "$", f"\n  {twtoscss_dict[splitted[1]]}\n"
-                    )
-                    + "\n"
-                )
-            elif splitted[0] in states:
-                scss_to_write = (
-                    "&"
-                    + states[splitted[0]].replace(
-                        "$", f"\n  {twtoscss_dict[splitted[1]]}\n"
-                    )
-                    + "\n"
-                )
-            print(scss_to_write)
-            out.write(scss_to_write)
-
-        if tw_class in twtoscss_dict:
-            scss_to_write = f"{twtoscss_dict[tw_class]}\n"
-            print(scss_to_write)
-            out.write(scss_to_write)
+            convert_derived_state(tw_class, twtoscss_dict, media_queries, states, out)
+        elif tw_class in twtoscss_dict:
+            convert_tw_class(tw_class, twtoscss_dict, out)
         else:
             # TODO: implement not tailwind class
             pass
@@ -65,28 +94,26 @@ def twjson_to_dict(file_name: str):
         return data
 
 
-def main():
-    file_name = sys.argv[1]
-
+def convert_tw_file_to_scss(in_file_name: str, dict_file_name: str, out_file_name: str):
     try:
-        scss_file = open(file_name, "r+")
+        scss_file = open(in_file_name, "r+")
     except IOError:
-        print(f"Cannot open this file: {file_name}")
+        print(f"Cannot open this file: {in_file_name}")
+        exit(-1)
 
     scss_lines = scss_file.read().split("\n")
 
-    twtoscss_dict = twjson_to_dict("tw_to_scss.json")
+    twtoscss_dict = twjson_to_dict(dict_file_name)
 
-    # go to start of file and delete all file content
-    # scss_file.seek(0)
-    # scss_file.truncate()
-    out_file = open("out.scss", "w")
+    with open(out_file_name, "w") as out_file:
+        for line in scss_lines:
+            convert_line_and_write(line, twtoscss_dict, out_file)
 
-    for line in scss_lines:
-        convert_line_and_write(line, twtoscss_dict, out_file)
-        continue
 
-    scss_file.close()
+def main():
+    file_name = sys.argv[1]
+
+    convert_tw_file_to_scss(file_name, DEFAULT_DICT_FILE_NAME, DEFAULT_OUT_FILT_NAME)
 
 
 if __name__ == "__main__":
